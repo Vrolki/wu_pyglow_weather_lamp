4f94a79077d43c9a3ba58f1f37fcba52222ec88b
Vrolki <tseril@gmail.com>
2015-06-17 03:44:47 -0400

    typo

--- wuSample.py
+++ wuSample.py
@@ -4,11 +4,10 @@
 
 import urllib2
 import json
-f = urllib2.urlopen('http://api.wunderground.com/api/<your_api>/geolookup/conditions/q/IA/Cedar_Rapids.json')
+f = urllib2.urlopen('http://api.wunderground.com/api/<your_api_key>/geolookup/conditions/q/IA/Cedar_Rapids.json')
 json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
 temp_f = parsed_json['current_observation']['temp_f']
 print "Current temperature in %s is: %s" % (location, temp_f)
 f.close()
-
